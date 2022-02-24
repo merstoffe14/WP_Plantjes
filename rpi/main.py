a1d4b0670fa898ab7a2e860d9def1aa36626267d
MerlijnStoffels <merlijn.qm@homail.com>
2022-02-24 17:52:25 +0100

    Worked on front end

--- rpi/main.py
+++ rpi/main.py
@@ -1,8 +1,4 @@
-from asyncore import loop
 from datetime import datetime
-import string
-from turtle import delay
-from typing import Optional
 from fastapi import FastAPI, Response
 from fastapi.responses import HTMLResponse
 import asyncio
@@ -11,42 +7,60 @@ from runner import BackgroundRunner
 from fastapi.responses import JSONResponse
 
 
+
 app = FastAPI()
 runner = BackgroundRunner()
 
 
 @app.on_event('startup')
 async def app_startup():
+    await runner.load_data()
     asyncio.create_task(runner.run_main())
 
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root():
-    f = open("index.html", "r")
-    html_content = f.read()
-    return HTMLResponse(content=html_content, status_code=200)
+    with open("index.html", "r") as f:
+        return HTMLResponse(content=f.read(), status_code=200)
 
+@app.get("/howto", response_class=HTMLResponse)
+async def read_howto():
+    with open("howto.html", "r") as f:
+        return HTMLResponse(content=f.read(), status_code=200)
 
 @app.get("/gettime")
 async def read_gettime():
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
-
     return current_time
+
 
 @app.get("/getdata")
 async def read_getdata():
+    await runner.load_data()
     payload = runner.plant_boxes
     return payload
 
 
 @app.post("/updatename")
-def update_plantbox_name(value: str, id: int):
+async def update_plantbox_name(value: str, id: int):
+    print(id,value)
+    await runner.load_data()
+    #Validate
+    if id not in runner.plant_boxes:
+        return JSONResponse(status_code=404, content={"error": "Plantbox not found"})
+    runner.update_name(id, value)
+    return Response(status_code=200)
+
+
+@app.post("/updatedate")
+async def update_plantbox_date(value: str, id: int):
+    await runner.load_data()
     # Validate
     if id not in runner.plant_boxes:
         return JSONResponse(status_code=404, content={"error": "Plantbox not found"})
 
-    runner.plant_boxes[id].name = value
+    runner.update_date(id, value)
     return Response(status_code=200)
-    
+
 
--- rpi/runner.py
+++ rpi/runner.py
@@ -1,6 +1,6 @@
 import asyncio
 from models import PlantBox
-
+import os,shutil,jsonpickle
 
 
 class BackgroundRunner:
@@ -8,18 +8,50 @@ class BackgroundRunner:
         self.water_requested = False
 
         self.plant_boxes = {
-            1: PlantBox(1)#,
-            #2: PlantBox(2),
-            #3: PlantBox(3),
-            #4: PlantBox(4)
+            1: PlantBox(1)  # ,
+            # 2: PlantBox(2),
+            # 3: PlantBox(3),
+            # 4: PlantBox(4)
         }
 
     async def run_main(self):
-       
-       while True:
+
+        while True:
             await asyncio.sleep(1)
             self.loop()
 
+    
+    # Loading data from locally saved file
+    async def load_data(self):
+        print("Loading data")
+
+    # Check if data.json exists, if not copy from hardcoded initial template.
+        try:
+            if not os.path.exists('data.json'):
+                shutil.copyfile('init_data.json', 'data.json')
+        except Exception as e:
+            raise Exception("Initial data json is missing!") from e
+
+    # Read database in memory.
+        with open('data.json', 'r') as json_file:
+            json_data = json_file.read()
+            self.plant_boxes = jsonpickle.decode(json_data)
+
+
+    # SAVE READ PROBLEEM CHECKEN
+    # Saving data to locally saved file
+    async def save_data(self):
+        print("Saving data")
+        with open('data.json', 'w') as json_file:
+            json_file.write(jsonpickle.encode(self.plant_boxes))
+
+    async def update_name(self, id: int, value: str):
+        self.plant_boxes[id].name = value
+        await self.save_data()
+
+    async def update_date(self, id: int, value: str):
+        self.plant_boxes[id].date = value
+        await self.save_data()
 
     def loop(self):
         pass
