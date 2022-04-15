8c50ca12532baf2fdf13bfc65c9607edb45bd71c
MerlijnStoffels <merlijn.qm@homail.com>
2022-04-15 16:48:09 +0200

    added gpio for pump/valves/lamps

--- rpi/runner.py
+++ rpi/runner.py
@@ -1,4 +1,5 @@
 import asyncio
+from pickle import FALSE
 from xml.dom.minidom import Element
 from models import PlantBox
 import os
@@ -8,6 +9,9 @@ from datetime import datetime
 import schedule
 import time
 import re
+import RPi.GPIO as GPIO
+
+
 
 
 class BackgroundRunner:
@@ -21,6 +25,27 @@ class BackgroundRunner:
         }
 
     def run_main(self):
+
+        self.time_pump_safety = 0.5
+        self.time_pump_prime = 0
+
+        self.lamp_1_pin = 1
+        self.lamp_2_pin = 7
+        self.lamp_3_pin = 8
+        self.lamp_4_pin = 25
+        self.valve_1_pin = 0
+        self.valve_2_pin = 5
+        self.valve_3_pin = 6
+        self.valve_4_pin = 13
+        self.pump_pin = 19
+
+        self.lamppin_array = [self.lamp_1_pin, self.lamp_2_pin, self.lamp_3_pin, self.lamp_4_pin]
+        self.valvepin_array = [self.valve_1_pin, self.valve_2_pin, self.valve_3_pin, self.valve_4_pin]
+        
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+        chan_list = [self.lamp_1_pin, self.lamp_2_pin, self.lamp_3_pin,  self.lamp_4_pin, self.valve_1_pin, self.valve_2_pin, self.valve_3_pin, self.valve_4_pin, self.pump_pin]  
+        GPIO.setup(chan_list, GPIO.OUT)
 
         while True:
             time.sleep(1)
@@ -53,15 +78,18 @@ class BackgroundRunner:
             json_file.write(jsonpickle.encode(self.plant_boxes))
 
 # nog de lampen zelf aan en uit doen, daarom de if
+
     def lamp(self, id: int, status: int):
         if status == 1:
-            self.plant_boxes[str(id)].light_status = 1
+            self.plant_boxes[str(id)].light_status = 1  
         elif status == 0:
             self.plant_boxes[str(id)].light_status = 0
         else:
             print("given status is not a valid status for lamp")
 
+        GPIO.output(self.lamppin_array[id-1], status)
         print("Turning " + str(id) + " " + str(status) + " gedoe")
+
 
 
 
@@ -70,9 +98,22 @@ class BackgroundRunner:
         now = datetime.now()
         self.plant_boxes[str(id)].last_spray = now.strftime("%H:%M:%S")
         print("Spraying at " + str(id) + " for: " + str(spraytime) + " seconds")
-        # relaysbrrrt
-        time.sleep(spraytime)
-        print("relays uit")
+
+        # Valve open
+        GPIO.output(self.valvepin_array[id-1], True)
+        # Small safety time, to open valve fully
+        time.sleep(self.time_pump_safety)
+        # Turn on pump
+        GPIO.output(self.pump_pin, True)
+        # Let pump run for spraytime + a priming time
+        time.sleep(spraytime + self.time_pump_prime)
+        # Turn of pump
+        GPIO.output(self.pump_pin, False)
+        # Wait for safety
+        time.sleep(self.time_pump_safety)
+        # Close valves
+        GPIO.output(self.valvepin_array[id-1], False)
+        print("Spray done")
 
 
     async def getwaterlevel():
